e326280d194acaa116b7bcd973074e65d6781b6d
thedumbterminal <github@thedumbterminal.co.uk>
2016-09-26 23:55:50 +0100

    can now be instantiate like any other app

--- app.psgi
+++ app.psgi
@@ -1,16 +1,6 @@
 use strict;
 use warnings;
-use CGI::Compile;
-use CGI::Emulate::PSGI;
-use Plack::Builder;
-use Plack::App::MCCS;
 use lib "lib";
+use Plack::App::WebMySQL;
 
-my $sub = CGI::Compile->compile("./cgi-bin/webmysql/webmysql.cgi");
-my $app = CGI::Emulate::PSGI->handler($sub);
-
-my $staticApp = Plack::App::MCCS->new(root => "./htdocs/webmysql")->to_app;
-builder {
-	mount "/webmysql" => $staticApp;	
-	mount "/" => $app;
-};
+my $app = Plack::App::WebMySQL->new()->to_app;
--- lib/Plack/App/WebMySQL.pm
+++ lib/Plack/App/WebMySQL.pm
@@ -1,11 +1,29 @@
 #the dumb terminal webmysql module
 #mt 16/11/2003 2.4	moved version into this module
 package Plack::App::WebMySQL;
+use strict;
+use warnings;
+use CGI::Compile;
+use CGI::Emulate::PSGI;
+use Plack::Builder;
+use Plack::App::MCCS;
 use Exporter();
 our %form;	#data from the previous page
 our $error = "";	#error flag
 our $VERSION ="3.0";	#version of this software
-@ISA = qw(Exporter);
-@EXPORT = qw(%form $error $VERSION);
+our @ISA = qw(Exporter);
+our @EXPORT = qw(%form $error $VERSION);
+###############################################################################
+sub new{
+	my $sub = CGI::Compile->compile("./cgi-bin/webmysql/webmysql.cgi");
+	my $app = CGI::Emulate::PSGI->handler($sub);
+
+	my $staticApp = Plack::App::MCCS->new(root => "./htdocs/webmysql")->to_app;
+		
+	my $builder = Plack::Builder->new();
+	$builder->mount("/webmysql" => $staticApp);	
+	$builder->mount("/" => $app);
+	return $builder;
+}
 ###############################################################################
 return 1;
